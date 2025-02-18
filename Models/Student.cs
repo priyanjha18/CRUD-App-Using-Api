1ae018af7d08d6f1b9500d7969203afae7e8178a
Priyan Jha <50802681+priyanjha18@users.noreply.github.com>
2025-02-19 04:47:08 +0530

    Add files via upload

---
+++ Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudAppUsingApi.Models
+{
+    
+        public class Student
+        {
+           
+            public int id { get; set; }
+            [Required]
+            public string studentName { get; set; }
+        [Required]
+        public string studentGender { get; set; }
+        [Required,Range(3,21)]
+        public int age { get; set; }
+        [Required,Range(1,12)]
+        public int standard { get; set; }
+        }
+
+    }
